1afbb61fef9fe0693a39830dd29e08b546c75bfb
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-02 20:32:45 +0200

    push-code

--- Shell-Commands/dotmart-install.sh
+++ Shell-Commands/dotmart-install.sh
@@ -1,17 +1,17 @@
-echo -e "\e[35m >>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<< \e[0m"
+echo -e "\e[31m >>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<< \e[0m"
 dnf install -y nginx
 
 echo -e "\e[35m >>>>>>>>>>>>> Copy Nginx Config <<<<<<<<<<<<<<< \e[0m"
 cp nginx.conf /etc/nginx/nginx.conf
 
-echo -e "\e[35m >>>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<<<< \e[0m"
+echo -e "\e[32m >>>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<<<< \e[0m"
 curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
 dnf install -y nodejs
 
-echo -e "\e[35m >>>>>>>>>>>>> Download Frontend Code <<<<<<<<<<<<<<< \e[0m"
+echo -e "\e[33m >>>>>>>>>>>>> Download Frontend Code <<<<<<<<<<<<<<< \e[0m"
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip
 
-echo -e "\e[35m >>>>>>>>>>>>> Create App Directory <<<<<<<<<<<<<<< \e[0m"
+echo -e "\e[34m >>>>>>>>>>>>> Create App Directory <<<<<<<<<<<<<<< \e[0m"
 rm -rf /tmp/frontend
 mkdir -p /tmp/frontend
 cd /tmp/frontend
@@ -24,7 +24,7 @@ npm cache clean --force
 npm install
 npm run build
 
-echo -e "\e[35m >>>>>>>>>>>>> Copy Built Code to Nginx <<<<<<<<<<<<<<< \e[0m"
+echo -e "\e[36m >>>>>>>>>>>>> Copy Built Code to Nginx <<<<<<<<<<<<<<< \e[0m"
 rm -rf /usr/share/nginx/html/*
 cp -r out/* /usr/share/nginx/html/
 
