f55ee926de8bfd8e3556c9a3891649627a939c2a
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-04 15:11:58 +0200

    updated4

--- Shell-Commands/redirect.sh
+++ Shell-Commands/redirect.sh
@@ -10,26 +10,26 @@ curl -fsSL https://rpm.nodesource.com/setup_20.x | bash - >>/tmp/roboshop.log 2>
 dnf install -y nodejs >>/tmp/roboshop.log 2>&1
 
 echo -e "\e[35m >>>>>>>>>>>>> Download Frontend Code <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
-curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip &>/tmp/roboshop.log
+curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[35m >>>>>>>>>>>>> Create App Directory <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
-rm -rf /tmp/frontend >/tmp/roboshop.log 2>&1
-mkdir -p /tmp/frontend >/tmp/roboshop.log 2>&1
-cd /tmp/frontend >/tmp/roboshop.log 2>&1
+rm -rf /tmp/frontend >>/tmp/roboshop.log 2>&1
+mkdir -p /tmp/frontend >>/tmp/roboshop.log 2>&1
+cd /tmp/frontend >>/tmp/roboshop.log 2>&1
 
 echo -e "\e[35m >>>>>>>>>>>>> Extract App Code <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 unzip /tmp/frontend.zip >>/tmp/roboshop.log 2>&1
 
 echo -e "\e[35m >>>>>>>>>>>>> Install App Dependencies & Build Html Code <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 npm cache clean --force >>/tmp/roboshop.log 2>&1
-npm install >/tmp/roboshop.log 2>&1
-npm run build >/tmp/roboshop.log 2>&1
+npm install >>/tmp/roboshop.log 2>&1
+npm run build >>/tmp/roboshop.log 2>&1
 
 echo -e "\e[35m >>>>>>>>>>>>> Copy Built Code to Nginx <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
-rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log 2>&1
-cp -r out/* /usr/share/nginx/html/ >/tmp/roboshop.log 2>&1
+rm -rf /usr/share/nginx/html/* >>/tmp/roboshop.log 2>&1
+cp -r out/* /usr/share/nginx/html/ >>/tmp/roboshop.log 2>&1
 
 echo -e "\e[35m >>>>>>>>>>>>> Start Nginx Service <<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
-systemctl restart nginx >/tmp/roboshop.log 2>&1
-systemctl enable nginx >/tmp/roboshop.log 2>&1
+systemctl restart nginx >>/tmp/roboshop.log 2>&1
+systemctl enable nginx >>/tmp/roboshop.log 2>&1
 
