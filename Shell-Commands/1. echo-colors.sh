9bf476b500b22e666d7f68c27879a237755a18df
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-05 16:50:39 +0200

    updated

--- Shell-Commands/1. echo-colors.sh
+++ Shell-Commands/1. echo-colors.sh
@@ -1,3 +1,10 @@
+echo Hello World
+
+echo -e \\e[31mHello World\\e[0m # Red Color
+echo -e "\e[31mHello World\e[0m" # prefer using double quotes
+echo -e '\e[31mHello World\e[0m' # Avoid using single quotes
+
+
 echo -e \\e[31mHello World\\e[0m # Red Color
 echo -e \\e[32mHello World\\e[0m # Green Color
 echo -e \\e[33mHello World\\e[0m # Yellow Color
--- Shell-Commands/2. echo.sh
+++
@@ -1,5 +0,0 @@
-echo Hello World
-
-echo -e \\e[31mHello World\\e[0m # Red Color
-echo -e "\e[31mHello World\e[0m" # prefer using double quotes
-echo -e '\e[31mHello World\e[0m' # Avoid using single quotes
