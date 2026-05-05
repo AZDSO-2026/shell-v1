3704c7f0de941dfb72f65e743e63ca588dd0f86a
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-05 18:49:14 +0200

    updated

--- Shell-Commands/7. functions.sh
+++ Shell-Commands/7. functions.sh
@@ -1,20 +1,9 @@
-function() {
+funcm() {
     echo Hello-World 
 }
 
-function
+funcm
 
 source 8. common.sh
 func1
 func2
-
-
-
-
-# Install_Nginx() {
-#     echo "Installing Nginx" 
-#     sudo dnf install -y nginx 
-#     sudo systemctl start nginx 
-# }
-
-# Install_Nginx
