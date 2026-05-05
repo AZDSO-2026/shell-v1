f421c783373458e5940e8acd406a104aeda03a4f
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-05 18:21:06 +0200

    updated

--- Shell-Commands/7. functions.sh
+++ Shell-Commands/7. functions.sh
@@ -1,7 +1,7 @@
 Install_Nginx() {
     echo "Installing Nginx" 
-    sudo dnf install -y nginx 
-    sudo systemctl start nginx 
+    sudo dnf install -y nginx  /tmp/file 2>&1 | tee -a /tmp/file
+    sudo systemctl start nginx  /tmp/file 2>&1 | tee -a /tmp/file
 }
 
 Install_Nginx
