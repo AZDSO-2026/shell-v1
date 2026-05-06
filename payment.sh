396bc1e20c021432be1675c380098318de5bc85d
SKR-2021 <karunakar.dev2000@gmail.com>
2026-05-06 20:13:09 +0200

    shipping

---
+++ payment.sh
@@ -0,0 +1,21 @@
+dnf install -y python3 python3-pip
+python3 --version
+
+cp payment.service /etc/systemd/system/payment.service
+
+useradd -r -s /bin/false appuser
+rm -rf /app
+mkdir -p /app
+
+curl -L -o /tmp/payment.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/payment.zip
+cd /app
+unzip /tmp/payment.zip
+pip3 install -r requirements.txt
+chown -R appuser:appuser /app
+chmod o-rwx /app -R
+
+systemctl daemon-reload
+systemctl enable payment
+systemctl restart payment
+
+systemctl status payment
